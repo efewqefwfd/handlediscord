7d875b31dfccbc42c8977fd85806dc8b56d9ef37
uhhhwdqdq <ryaninc31@gmail.com>
2018-02-24 13:28:23 -0700

    Update mainfile.js

--- mainfile.js
+++ mainfile.js
@@ -1,6 +1,11 @@
 var express = require('express')
 var app = express()
  
-app.post('/', function (req, res) {
-  res.send('Got a POST request')
-})
+app.get('/discord/:hook', function (req, res, next) {
+ var hook = req.params.hook;
+ console.log(hook)
+}
+        
+        app.listen(port, function () {
+    console.log('Listening on port ' + port);
+  });
