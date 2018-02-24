a10f9e57980fba7098be0803174845837fa69363
uhhhwdqdq <ryaninc31@gmail.com>
2018-02-24 14:24:18 -0700

    Update mainfile.js

--- mainfile.js
+++ mainfile.js
@@ -3,7 +3,7 @@ var Webhook = require("webhook-discord")
 var app = express()
 var port = process.env.PORT || 8080;
 
-app.get('/discord/:hook/:name/:content', function (req, res, next) {
+app.get('/hook/:hook/name/:name/content/:content', function (req, res, next) {
  console.log('hi')
  var hook = req.params.hook;
  var name = req.params.name;
