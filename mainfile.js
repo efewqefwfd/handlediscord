9d8d53b9168a055ab079361ad61f6de292b987cf
uhhhwdqdq <ryaninc31@gmail.com>
2018-02-24 13:29:41 -0700

    Update mainfile.js

--- mainfile.js
+++ mainfile.js
@@ -3,8 +3,8 @@ var app = express()
  
 app.get('/discord/:hook', function (req, res, next) {
  var hook = req.params.hook;
- console.log(hook)
-}
+ console.log(hook);
+});
         
         app.listen(port, function () {
     console.log('Listening on port ' + port);
