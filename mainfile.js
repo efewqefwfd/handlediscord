525585acd9c0b70cb66826495c757c0a3885eb58
uhhhwdqdq <ryaninc31@gmail.com>
2018-02-25 13:18:07 -0700

    Update mainfile.js

--- mainfile.js
+++ mainfile.js
@@ -1,5 +1,22 @@
-const http = require('http');
-const server = http.createServer();
-server.on('request', (request, response) => {
-  console.log('hihi')
+
+
+
+var express = require('express');
+var fs = require('fs');
+var app = express();
+
+app.use(express.bodyParser());
+
+app.get('/', function(req, res){
+    console.log('GET /')
+    //var html = '<html><body><form method="post" action="http://localhost:3000">Name: <input type="text" name="name" /><input type="submit" value="Submit" /></form></body>';
 });
+
+app.post('/', function(req, res){
+    console.log('POST /');
+    console.dir(req.body); 
+});
+
+port = 3000;
+app.listen(port);
+console.log('Listening at http://localhost:' + port)
