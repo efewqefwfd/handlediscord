e4b84dd9c53ed3c2a2d5de5957e88e12dccc7265
uhhhwdqdq <ryaninc31@gmail.com>
2018-03-01 15:55:36 -0700

    Update mainfile.js

--- mainfile.js
+++ mainfile.js
@@ -1,6 +1,6 @@
 var express = require('express');
 app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!')).catch(console.error)
 
-app.listen(80, () => console.log('Example app listening on port 80!'))
+app.listen(80, () => console.log('Example app listening on port 80!')).catch(console.error)
