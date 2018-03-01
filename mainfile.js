301d8647663877c243856d84555e6c9a94cb5eb0
uhhhwdqdq <ryaninc31@gmail.com>
2018-03-01 14:32:25 -0700

    Update mainfile.js

--- mainfile.js
+++ mainfile.js
@@ -23,6 +23,22 @@ console.log('Listening at http://localhost:' + port)
 
 var request = require("request");
 
-request("http://www.sitepoint.com", function(error, response, body) {
-  console.log(body);
-});
+var http = require('http');
+var qs = require('querystring');
+
+var serverPort = 8124;
+http.createServer(function (request, response) {
+  if(request.method === "GET") {
+   console.log('get')
+  } else if(request.method === "POST") {
+      console.log('post')
+    if (request.url === "/cool") {
+        console.log('cool post')
+      request.on('data', function(data) {
+        console.log(data)
+      });
+      request.on('end', function() {
+       console.log('ended')
+      });
+}).listen(serverPort);
+console.log('Server running at localhost:'+serverPort);
