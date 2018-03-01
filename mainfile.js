6a8db4e31488fbaa1f3de0932e822df5ec9bd9d6
uhhhwdqdq <ryaninc31@gmail.com>
2018-03-01 14:50:48 -0700

    Update mainfile.js

--- mainfile.js
+++ mainfile.js
@@ -1,10 +1,38 @@
-var http = require('http');
-http.createServer(function (req, resp) {
-  if (req.url === '/post') {
-    if (req.method === 'POST') {
-     console.log('post')
-    } else if (req.method === 'GET' || req.method === 'HEAD') {
-      console.log('get')
-    }
-  }
-})
+//require the express nodejs module
+var express = require('express'),
+    //set an instance of exress
+    app = express(),
+    //require the body-parser nodejs module
+    bodyParser = require('body-parser'),
+    //require the path nodejs module
+    path = require("path");
+
+//support parsing of application/json type post data
+app.use(bodyParser.json());
+
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//tell express that www is the root of our public web folder
+app.use(express.static(path.join(__dirname, 'www')));
+
+//tell express what to do when the /about route is requested
+app.post('/POST', function(req, res){
+    //mimic a slow network connection
+    setTimeout(function(){
+
+        res.send(JSON.stringify({
+            firstName: req.body.firstName || null,
+            lastName: req.body.lastName || null
+        }));
+
+    }, 1000)
+
+    //debugging output for the terminal
+    console.log('you posted: First Name: ' + req.body.firstName + ', Last Name: ' + req.body.lastName);
+});
+
+//wait for a connection
+app.listen(3000, function () {
+  console.log('Server is running. Point your browser to: http://localhost:3000');
+});
