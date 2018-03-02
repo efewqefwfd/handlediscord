7d12d18b295aa0e02263c178be300a0cbee9d77a
uhhhwdqdq <ryaninc31@gmail.com>
2018-03-01 18:00:24 -0700

    Update mainfile.js

--- mainfile.js
+++ mainfile.js
@@ -1,6 +1,11 @@
+//JS Side
 var express = require('express');
 app = express();
+var port = process.env.PORT || 80
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.post('/', function (req, res) {
+res.send('Hello World!') 
+console.log('Post.')
+});
 
-app.listen(80, () => console.log('Example app listening on port 80!'))
+app.listen(80, () => console.log('Example app listening on port '+ port))
